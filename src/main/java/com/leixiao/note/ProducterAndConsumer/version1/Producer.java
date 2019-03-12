d2847138dba33057ec00e1e07e6102766d0b57f6
leixiao <2399520027@qq.com>
2019-03-12 17:08:02 +0800

    Producter and Consumer

--- src/main/java/com/leixiao/note/ProducterAndConsumer/version1/Producer.java
+++ src/main/java/com/leixiao/note/ProducterAndConsumer/version1/Producer.java
@@ -1,6 +1,8 @@
 package com.leixiao.note.ProducterAndConsumer.version1;
 
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Producer implements Runnable {
@@ -16,6 +18,26 @@ public class Producer implements Runnable {
 
     @Override
     public void run() {
+        PCData data = null;
+        Random r = new Random();
+        System.out.println("Start Producting id: " + Thread.currentThread().getId());
+        try{
+            while (isRunning){
+                Thread.sleep(r.nextInt(SLEEPTIME));
+                data = new PCData(count.incrementAndGet());
+                System.out.println(data+"加入队列");
+                if (!queue.offer(data,2, TimeUnit.SECONDS)){
+                    System.out.println("加入队列失败");
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            //抛出异常，将让线程中断
+            Thread.currentThread().interrupt();
+        }
+    }
 
+    public void stop(){
+        isRunning = false;
     }
 }
